e9100c6d9e726c2a94bff5c166d610c362085449
Quang Torres <doquang123400@gmail.com>
2022-03-29 10:36:59 -0500

    Update March 29 2022

--- js/test.js
+++ js/test.js
@@ -1,4 +1,8 @@
 window.onload = function () {
     var html = document.getElementsByTagName("html")[0];
     html.classList.add("transition-animation");
+    AOS.init({
+        duration: 1500,
+        once: true,
+    });
 }   //End window.onload
